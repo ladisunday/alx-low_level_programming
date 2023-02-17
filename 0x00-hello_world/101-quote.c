3b29caf40ee6d021d03792a7a787227f144d5bb0
ladisunday <ladisunday@gmail.com>
2023-02-16 19:30:47 -0800

    Print in standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print in standard error
+ *
+ * Return: One
+ */
+int main(void)
+{
+	-v("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
+	return (1);
+}
