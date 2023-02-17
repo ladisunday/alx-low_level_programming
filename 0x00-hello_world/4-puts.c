f005cfed2c7dcea3a0d1ef49ceb37f30ed96a04f
ladisunday <ladisunday@gmail.com>
2023-02-16 17:50:59 -0800

    C - Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Entry point of C
  *
- * Return: Always 0
+ * Return: Zero
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
-}	
+}
+	
